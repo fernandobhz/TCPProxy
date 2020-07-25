557cf1d4cd680c2c60f22dd6087797c12cca6d7a
Fernando Reis Guimarães <fernandobhz@gmail.com>
2020-07-25 19:19:57 -0300

    old project

---
+++ TCPProxy/Classes/TCPProxy.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Net.Sockets
+
+Class TCPProxy
+    Private _HostPort As Integer
+    Private _RemoteAdress As String
+    Private _RemotePort As Integer
+
+    Private Clients As New List(Of TCPProxyClient)
+    Private Server As TcpListener
+
+    Public Shared Async Function Start(HostPort As Integer, RemoteAddress As String, RemotePort As Integer) As Task
+        Dim X As New TCPProxy(HostPort, RemoteAddress, RemotePort)
+        Await X.Start()
+    End Function
+
+    Private Sub New(HostPort As Integer, RemoteAddress As String, RemotePort As Integer)
+        _HostPort = HostPort
+        _RemoteAdress = RemoteAddress
+        _RemotePort = RemotePort
+    End Sub
+
+    Private Async Function Start() As Task
+        Server = New TcpListener(Net.IPAddress.Any, _HostPort)
+        Server.Start()
+
+        Dim TcpClient As New TcpClient
+
+        While True
+            Try
+                TcpClient = Await Server.AcceptTcpClientAsync
+                Dim Handle As New TCPProxyClient(TcpClient, _RemoteAdress, _RemotePort)
+                Clients.Add(Handle)
+                Await Handle.Start()
+            Catch ex As Exception
+#If DEBUG Then
+                Debug.Assert(False)
+#End If
+            End Try
+        End While
+    End Function
+
+End Class
---
+++ TCPProxy/Main.vb
@@ -0,0 +1,28 @@
+﻿Module Main
+
+    Sub Main()
+
+        Dim Args() = Environment.GetCommandLineArgs()
+
+        Dim localPort As Integer
+        Dim remoteHost As String
+        Dim remotePort As Integer
+
+        Select Case Args.Count
+            Case 4
+                localPort = Args(1)
+                remoteHost = Args(2)
+                remotePort = Args(3)
+            Case 1
+                localPort = InputBox("Local port")
+                remoteHost = InputBox("Remote host")
+                remotePort = InputBox("Remote port")
+            Case Else
+                Console.WriteLine("Usage TCPProxy localport remoteHost remotePort")
+                Exit Sub
+        End Select
+
+        TCPProxy.Start(localPort, remoteHost, remotePort).Wait()
+    End Sub
+
+End Module
